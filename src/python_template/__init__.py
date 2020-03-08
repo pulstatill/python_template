82947675cb553788b958863932336f03dd3e41bb
billi <philipp.schoder@yahoo.de>
2020-03-08 12:29:37 +0100

    Initial commit

---
+++ src/python_template/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for python_template."""
+
+
+from ._version import get_versions
+__author__ = """Philipp Schoder"""
+__email__ = 'philipp.schoder@yahoo.de'
+__version__ = get_versions()['version']
+del get_versions
